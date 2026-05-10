c9b0bdcbb2be2e650cc9094ce8cf42c1625e3fe4
Tomfelkerytube <tomsfelkers2016@gmail.com>
2026-05-10 12:10:48 +0300

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,16 @@
+Write-Host "Downloading MaxOS..."
+
+$root = "$env:USERPROFILE\.maxkernel"
+
+New-Item -ItemType Directory -Force -Path $root | Out-Null
+
+Invoke-WebRequest `
+"https://github.com/Tomfelkerytube/MaxOS/releases/download/maxos-ver1/kernel.bat" `
+-OutFile "$root\kernel.bat"
+
+if (Test-Path "$root\kernel.bat") {
+    Write-Host "Starting MaxKernel..."
+    Start-Process "$root\kernel.bat"
+} else {
+    Write-Host "Failed to download MaxOS."
+}
